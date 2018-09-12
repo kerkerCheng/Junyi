0471e8c9ac1699520e3c50f9da67c6f3efd11525
kerkercheng <c3035281@gmail.com>
2018-09-12 17:35:03 +0800

    ok user rate mapping is ok.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import scipy.io as sio
+import scipy.io as so
 import numpy as np
 import src
 import collections
@@ -7,24 +7,34 @@ import collections
 num_sec = 338
 
 df_main = pd.read_csv('hash_junyi_section_learning_data.csv')
+df_val = df_main.values
 
-df1 = df_main.drop(['exercise', 'difficulty', 'curriculum_guideline_id', 'section_ex_cnt'], axis=1)
-df1 = df1.sort_values(['user_primary_key', 'section_title'], ascending=[True, False])
-df1_val = df1.values
+ok_user_list = np.load('morethan100_section_userslist.npy').tolist()
+all_user_list = np.load('all_user_list.npy').tolist()
+sec_list = np.load('section_list.npy').tolist()
 
-dd = collections.defaultdict(list)
-dd_to_unique_num = {}
+ok_df = pd.read_csv('more_than_100_sec_users.csv')
+ok_df = ok_df.sort_values(['user_primary_key', 'section_title'], ascending=[True, False])
+ok_val = ok_df.values
 
-for i in range(df1_val.shape[0]):
-    dd[df1_val[i][0]].append(df1_val[i][1])
+tensor = {}
+count = {}
+rate = {}
 
-for key, item in dd.items():
-    dd_to_unique_num[key] = len(np.unique(item))
+for i in range(len(ok_user_list)):
+    tensor[ok_user_list[i]] = np.zeros((338, 338))
+    count[ok_user_list[i]] = np.zeros(338)
+    rate[ok_user_list[i]] = np.zeros(338)
 
-th = 50
-ok_user_list = []
+for i in range(ok_val.shape[0]):
+    user_this = ok_val[i][0]
+    sec_this = ok_val[i][2]
+    (count[user_this])[sec_list.index(sec_this)] += 1
 
-for key, item in dd_to_unique_num.items():
-    if item > th:
-        ok_user_list.append(key)
+    if ok_val[i][5] is True:
+        (rate[user_this])[sec_list.index(sec_this)] += 1
 
+for key, value in rate.items():
+    for i in range(338):
+        if (count[key])[i] != 0:
+            value[i] /= (count[key])[i]
