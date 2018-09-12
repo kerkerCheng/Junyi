e9f31dad34d293cc367212933e108a6adf699754
kerkercheng <c3035281@gmail.com>
2018-09-12 22:12:49 +0800

    tensor is constructed

--- main.py
+++ main.py
@@ -17,24 +17,34 @@ ok_df = pd.read_csv('more_than_100_sec_users.csv')
 ok_df = ok_df.sort_values(['user_primary_key', 'section_title'], ascending=[True, False])
 ok_val = ok_df.values
 
-tensor = {}
-count = {}
-rate = {}
+rate = np.load('ok_user_rate_mapping.npy').item()
 
-for i in range(len(ok_user_list)):
-    tensor[ok_user_list[i]] = np.zeros((338, 338))
-    count[ok_user_list[i]] = np.zeros(338)
-    rate[ok_user_list[i]] = np.zeros(338)
+tensor = np.zeros((len(ok_user_list), 338, 338))
+# count = {}
+# rate = {}
+#
+# for i in range(len(ok_user_list)):
+#     tensor[ok_user_list[i]] = np.zeros((338, 338))
+#     count[ok_user_list[i]] = np.zeros(338)
+#     rate[ok_user_list[i]] = np.zeros(338)
+#
+# for i in range(ok_val.shape[0]):
+#     user_this = ok_val[i][0]
+#     sec_this = ok_val[i][2]
+#     (count[user_this])[sec_list.index(sec_this)] += 1
+#
+#     if ok_val[i][5] is True:
+#         (rate[user_this])[sec_list.index(sec_this)] += 1
+#
+# for key, value in rate.items():
+#     for i in range(338):
+#         if (count[key])[i] != 0:
+#             value[i] /= (count[key])[i]
 
-for i in range(ok_val.shape[0]):
-    user_this = ok_val[i][0]
-    sec_this = ok_val[i][2]
-    (count[user_this])[sec_list.index(sec_this)] += 1
-
-    if ok_val[i][5] is True:
-        (rate[user_this])[sec_list.index(sec_this)] += 1
-
+k = 0
 for key, value in rate.items():
     for i in range(338):
-        if (count[key])[i] != 0:
-            value[i] /= (count[key])[i]
+        for j in range(338):
+            tensor[k][i][j] = (value[i]*value[j])**0.5
+    k += 1
+
